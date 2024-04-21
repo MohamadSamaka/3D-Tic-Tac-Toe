b296ca27a52c1329e8c093906c6fef4e04b42341
Mohamad Samaka <mhmdkais20@gmail.com>
2024-04-21 04:31:56 +0300

    changed defaults

--- app/src/defaults.py
+++ app/src/defaults.py
@@ -3,13 +3,13 @@ from dotenv import load_dotenv
 from .helpers.helpers import get_style
 from .helpers.shared import Shared
 
-load_dotenv(r"D:\MyCodes\AWS projects\Tic-Tac-Toe\.env")  # Specify the exact path if not in the same directory
+load_dotenv(Shared.APP_ROOT_PATH.parent / ".env")  # Specify the exact path if not in the same directory
 
 try:
-    WINDOW_WIDTH = int(os.getenv('WINDOW_WIDTH', '1200'))
-    WINDOW_HEIGHT = int(os.getenv('WINDOW_HEIGHT', '900'))
-    # WINDOW_WIDTH = 1200
-    # WINDOW_HEIGHT = 1200
+    # WINDOW_WIDTH = int(os.getenv('WINDOW_WIDTH', '1200'))
+    # WINDOW_HEIGHT = int(os.getenv('WINDOW_HEIGHT', '1200'))
+    WINDOW_WIDTH = 1200
+    WINDOW_HEIGHT = 1200
     WIDTH_PARTITIONING_PRECENT= WINDOW_WIDTH * int(os.getenv('WIDTH_PARTITIONING_PRECENT', 20)) // 100
     HEIGHT_PARTITIONING_PRECENT = WINDOW_HEIGHT * int(os.getenv('HEIGHT_PARTITIONING_PRECENT', 20)) // 100
     TIMER = int(os.getenv('TIMER', 30))
